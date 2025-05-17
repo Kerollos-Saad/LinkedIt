98b85156133cfdfec0148e9ee9421933c02e1a02
Kerollos-Saad <kirloussaad7788@gmail.com>
2025-05-17 18:54:43 +0300

    Login & Register at User Repo

--- LinkedIt.DataAcess/Repository/IRepository/IUserRepository.cs
+++ LinkedIt.DataAcess/Repository/IRepository/IUserRepository.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using LinkedIt.Core.Models.User;
+using LinkedIt.Core.DTOs.Authentication;
+using LinkedIt.Core.DTOs.User;
 
 namespace LinkedIt.DataAcess.Repository.IRepository
 {
@@ -12,5 +14,11 @@ namespace LinkedIt.DataAcess.Repository.IRepository
 		Task<bool> IsUniqueUserName(string userName);
 		Task<ApplicationUser> GetUserById(string userId);
 		Task<bool> UpdateAsync(ApplicationUser user);
+
+		// Authentication
+		Task<LoginResponseDTO> Login(LoginRequestDTO  loginRequestDTO);
+		Task<UserDTO> Register(RegisterRequestDTO registerRequestDTO);
+		
+
 	}
 }
--- LinkedIt.DataAcess/Repository/UserRepository.cs
+++ LinkedIt.DataAcess/Repository/UserRepository.cs
@@ -1,13 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using LinkedIt.Core.DTOs.Authentication;
+using LinkedIt.Core.DTOs.User;
 using LinkedIt.Core.Models.User;
 using LinkedIt.DataAcess.Context;
 using LinkedIt.DataAcess.Repository.IRepository;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace LinkedIt.DataAcess.Repository
 {
@@ -21,12 +27,17 @@ namespace LinkedIt.DataAcess.Repository
 		private readonly IConfiguration _configuration; // To Get API Key
 		private string securityKey;
 
-		public UserRepository(ApplicationDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, IConfiguration configuration) : base(db)
+		// Authentication
+		private readonly IMapper _mapper;
+
+		public UserRepository(ApplicationDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, 
+			IConfiguration configuration, IMapper mapper) : base(db)
 		{
 			this._db = db;
 			this._userManager = userManager;
 			this._roleManager = roleManager;
 			this._configuration = configuration;
+			this._mapper = mapper;
 
 			//Need To install Package `Microsoft.Extensions.Configuration.Binder` and the method `GetValue` will be available
 			securityKey = _configuration.GetValue<string>("ApiSettings:Secret") ??
@@ -57,5 +68,92 @@ namespace LinkedIt.DataAcess.Repository
 			var result = await _db.SaveChangesAsync();
 			return result > 0;
 		}
+
+		public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+		{
+			var user = await _userManager.FindByNameAsync(loginRequestDTO.UserName);
+			if (user == null || !await _userManager.CheckPasswordAsync(user, loginRequestDTO.Password))
+			{
+				return new LoginResponseDTO()
+				{
+					Token = "",
+					User = null
+				};
+			}
+
+			var userRoles = await _userManager.GetRolesAsync(user);
+
+			var claims = new List<Claim>
+			{
+				new Claim(ClaimTypes.NameIdentifier, user.Id),
+				new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+				new Claim(ClaimTypes.Name, user.UserName)
+			};
+
+			claims.AddRange(userRoles.Select(r => new Claim(ClaimTypes.Role, r)));
+
+			var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(securityKey));
+			var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+			var token = new JwtSecurityToken(
+				claims: claims,
+				expires: DateTime.UtcNow.AddHours(6),
+				signingCredentials: credentials
+			);
+
+			return new LoginResponseDTO()
+			{
+				Token = new JwtSecurityTokenHandler().WriteToken(token),
+				User = _mapper.Map<UserDTO>(user)
+			};
+		}
+
+		public async Task<UserDTO> Register(RegisterRequestDTO registerRequestDTO)
+		{
+			var user = new ApplicationUser
+			{
+				UserName = registerRequestDTO.UserName,
+				FirstName = registerRequestDTO.FirstName,
+				LastName = registerRequestDTO.LastName,
+				Email = registerRequestDTO.Email,
+				NormalizedEmail = registerRequestDTO.Email.ToUpper()
+			};
+
+			var userDTO = new UserDTO();
+
+			try
+			{
+				var result = await _userManager.CreateAsync(user, registerRequestDTO.Password);
+				if (result.Succeeded)
+				{
+					if (registerRequestDTO.Roles != null && registerRequestDTO.Roles.Any())
+					{
+						foreach (var role in registerRequestDTO.Roles)
+						{
+							if (!await _roleManager.RoleExistsAsync(role))
+							{
+								await _roleManager.CreateAsync(new IdentityRole(role));
+							}
+
+							await _userManager.AddToRoleAsync(user, role);
+						}
+					}
+
+					userDTO = _mapper.Map<UserDTO>(user);
+				}
+				else
+				{
+					userDTO.ErrorMessages = result.Errors.Select(e => e.Description).ToList();
+				}
+			}
+			catch (Exception ex)
+			{
+				userDTO.ErrorMessages = new List<string> { "An unexpected error occurred while registering the user." };
+			}
+
+			return userDTO;
+		}
+
+		
 	}
 }
