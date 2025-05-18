97fae8e7376f3a1a4221e36d922931141368ceb0
Kerollos-Saad <kirloussaad7788@gmail.com>
2025-05-18 16:20:18 +0300

    Enable Swagger UI

---
+++ LinkedIt.API/Controllers/AuthUserController.cs
@@ -0,0 +1,26 @@
+﻿using LinkedIt.Services.ControllerServices.IControllerServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LinkedIt.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class AuthUserController : ControllerBase
+	{
+
+		private readonly IAuthService _authService;
+
+		public AuthUserController(IAuthService authService)
+		{
+			this._authService = authService;
+		}
+
+		[HttpGet("hi")]
+		public async Task<IActionResult> Hi()
+		{
+			return Ok();
+		}
+
+	}
+}
--- LinkedIt.API/Program.cs
+++ LinkedIt.API/Program.cs
@@ -77,16 +77,20 @@ namespace LinkedIt.API
 
 			// Add services to the container.
 			builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
 
-            var app = builder.Build();
+            // Register Swagger services
+            builder.Services.AddEndpointsApiExplorer(); // Required
+            builder.Services.AddSwaggerGen();           // Required
+
+			var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
-            }
+                // Enable Swagger
+                app.UseSwagger();
+                app.UseSwaggerUI();
+			}
 
             app.UseAuthentication();
             app.UseAuthorization();
